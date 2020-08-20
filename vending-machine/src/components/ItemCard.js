005350a951fcc2003d930b7ee3cf308871e0c611
buchidiai <31743169+buchidiai@users.noreply.github.com>
2020-08-20 14:29:04 -0700

    init

---
+++ vending-machine/src/components/ItemCard.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const ItemCard = () => {
+  return (
+    <div>
+      <p>1</p>
+      <p>M & M</p>
+      <p>$1.50</p>
+      <p>Quantity left: 9</p>
+    </div>
+  );
+};
---
+++ vending-machine/src/page/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+// import { Container } from "react-bootstrap/Container";
+import { ItemCard } from "../components/ItemCard";
+import Container from "react-bootstrap/Container";
+
+function App() {
+  return (
+    <Container>
+      <ItemCard />
+    </Container>
+  );
+}
+
+export default App;
